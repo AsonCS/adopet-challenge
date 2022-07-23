53bface637cd34043a1c30d1790096ddf4840bcf
anderson.silva.agi <RtEJD2ATyzxuuXFdS7KD>
2022-07-22 22:26:38 -0300

    Fix Week 3 and 4

--- src/api/repository/remote/index.ts
+++ src/api/repository/remote/index.ts
@@ -1,9 +1,7 @@
 import Pets from '../../model/Pets'
 
 export const getPets = async (): Promise<Pets> => {
-	const response: Pets = await fetch(
-		'https://adopet-by-asoncs.web.app/json/pets.json'
-	)
+	const response: Pets = await fetch('/json/pets.json')
 		.then((value) => value.json())
 		.then((value) => new Pets(value))
 		// eslint-disable-next-line handle-callback-err, @typescript-eslint/no-unused-vars
